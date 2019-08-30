edc41b5e583714142152d4920cd7b6f48c247811
Viacheslav Germonenko <vi.germonenko@outlook.com>
2019-08-30 22:01:53 +0300

    Project configured.
    * Packages configured.
    * Dev tools configured.

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: {
+    main: './src/index.js',
+  },
+
+  output: {
+    path: path.resolve(__dirname, '..', 'build'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        exclude: /node_modules/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      favicon: 'assets/favicon.ico',
+    }),
+  ],
+
+  devServer: {
+    port: 3000,
+    
+  },
+
+};
