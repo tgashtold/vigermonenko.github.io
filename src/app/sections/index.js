627bf3165432047e4f96492d85bd175d686b438f
Viacheslav Germonenko <vi.germonenko@outlook.com>
2019-08-31 00:55:21 +0300

    All sections exported from one file.

---
+++ src/app/sections/index.js
@@ -0,0 +1,9 @@
+import SearchSection from './SearchSection';
+import ResultSection from './ResultSection';
+import InfoSection from './InfoSection';
+
+export {
+  SearchSection,
+  ResultSection,
+  InfoSection,
+};
